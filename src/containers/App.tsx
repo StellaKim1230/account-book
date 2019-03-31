363f50b709f4b4f6d14a69799766e0a9e0524710
Stellakim1230 <stellakim1230@gmail.com>
2019-03-31 21:33:34 +0900

    delete console.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react'
 
 import { BrowserRouter, Route } from 'react-router-dom'
-// import { Router, Route } from 'react-router'
 
 import SiderbarTemplate from '../templates/SiderbarTemplate'
 import MainPage from '../pages/MainPage'
